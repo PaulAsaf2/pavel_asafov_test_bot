b796ac08818917b2747977f007faa04a8e8b6eb2
Pavel Asafov <pavelaof@gmail.com>
2024-08-14 18:51:57 +0300

    add magnettopro

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const testWebApp = InlineKeyboard.webApp('test webapp', 'https://domen-a.ru');
 const wallstringDev = InlineKeyboard.webApp('WS Dev | nodeapi.ru', 'https://nodeapi.ru/?promocode=0ZL-N88-CWZ-BD3');
 const cropImageProd = InlineKeyboard.webApp('wallstring.monitour.ru/crop', 'https://wallstring.monitour.ru/crop/?promocode=0ZL-N88-CWZ-BD3');
 const wallstringProd = InlineKeyboard.webApp('wallstring.monitour.ru/voice', 'https://wallstring.monitour.ru/voice/?promocode=0ZL-N88-CWZ-BD3');
+const magnettopro = InlineKeyboard.webApp('Magnetto.pro | magnetto.monitour.ru', 'https://magnetto.monitour.ru');
 
-const buttons = [testWebApp, wallstringDev, cropImageProd, wallstringProd];
+const buttons = [testWebApp, wallstringDev, cropImageProd, wallstringProd, magnettopro];
 const keyboard = InlineKeyboard.from(buttons.map(button => [button]));
 
 bot.command('start', ctx => ctx.reply('Welcome!'));
