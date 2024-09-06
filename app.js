82f9839777b54dcb6c98131c591aaa5535c0f636
Pavel Asafov <pavelaof@gmail.com>
2024-09-06 09:42:20 +0300

    add basic doodle

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Bot, InlineKeyboard } = require('grammy');
 const bot = new Bot(process.env.TOKEN);
 
+const basicDoodle = InlineKeyboard.webApp('basic doodle | nodeapi.ru', 'https://nodeapi.ru/basic-doodle/');
 const doodle = InlineKeyboard.webApp('doodle | nodeapi.ru', 'https://nodeapi.ru/dooodle/');
 const certificateDev = InlineKeyboard.webApp('certificate | nodeapi.ru', 'https://nodeapi.ru/certificate/?lead=1234567');
 const testWebApp = InlineKeyboard.webApp('test webapp', 'https://domen-a.ru');
@@ -11,14 +12,23 @@ const wallstringProd = InlineKeyboard.webApp('wallstring.monitour.ru/voice', 'ht
 const magnettopro = InlineKeyboard.webApp('Magnetto.pro | magnetto.monitour.ru', 'https://magnetto.monitour.ru');
 const magnettoproDev = InlineKeyboard.webApp('Magnetto.pro | domen-a.ru', 'https://domen-a.ru/magnettopro/');
 
-const buttons = [doodle, certificateDev, testWebApp, wallstringDev, cropImageProd, wallstringProd, magnettopro, magnettoproDev];
+const buttons = [certificateDev, testWebApp, wallstringDev, cropImageProd, wallstringProd, magnettopro, magnettoproDev];
 const keyboard = InlineKeyboard.from(buttons.map(button => [button]));
+
+const gameButtons = [doodle, basicDoodle];
+const gameKeyboard = InlineKeyboard.from(gameButtons.map(button => [button]));
 
 bot.command('start', ctx => ctx.reply('Welcome!'));
 
 bot.command('webapp', async (ctx) => {
   await ctx.reply('Webapps', {
     reply_markup: keyboard
+  });
+});
+
+bot.command('game', async (ctx) => {
+  await ctx.reply('Webapps', {
+    reply_markup: gameKeyboard
   });
 });
 
