3c5676859e0902fed9566cac37e4af03c563048d
Pavel Asafov <pavelaof@gmail.com>
2024-10-18 18:58:54 +0300

    add test29.ru

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Bot, InlineKeyboard } = require('grammy');
 const bot = new Bot(process.env.TOKEN);
 
+const tgAdsTest = InlineKeyboard.webApp('Telegram Ads | test29.ru', 'https://test29.ru/tg-ads/');
 const tgAds = InlineKeyboard.webApp('Telegram Ads | localhost', 'https://192.168.0.103:3000/tg-ads/');
 // const testWebApp = InlineKeyboard.webApp('test webapp', 'https://domen-a.ru');
 // const wallstringDev = InlineKeyboard.webApp('WS Dev | nodeapi.ru', 'https://nodeapi.ru/?promocode=0ZL-N88-CWZ-BD3');
